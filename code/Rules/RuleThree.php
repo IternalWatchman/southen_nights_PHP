9e47ebe2271c2c8a4eee75528b65968496499aaa
Kauperwood <noblethrow@gmail.com>
2017-10-05 06:31:53 +0100

    discount rules - step 2

--- code/Rules/RuleThree.php
+++ code/Rules/RuleThree.php
@@ -7,9 +7,25 @@ use Lex\Order;
 
 class RuleThree implements Rule {
 	public function test(Order $order) {
-		return false;
+		$quantity = 0;
+		foreach($order->products() as $item) {
+			if($item->category() == 1) {
+				$quantity += $item->quantity();
+			}
+		}
+		return $quantity >= 2;
 	}
 	public function calc(Order $order) {
-		return false;
+		$prices = array();
+		foreach($order->products() as $item) {
+			if($item->category() == 1)
+				array_push($prices, $item->price());
+		}
+
+		$minimum = min($prices);
+		return array(
+			'discount' => $minimum * .20,
+			'description' => "If you buy two or more products of category 'Tools' (id 1), ou get a 20% discount on the cheapest product."
+		);
 	}
 }
--- code/Rules/RuleTwo.php
+++ code/Rules/RuleTwo.php
@@ -7,10 +7,22 @@ use Lex\Order;
 
 class RuleTwo implements Rule {
 	public function test(Order $order) {
-		return false;
-
+		return count(array_filter($order->products(), function($item) {
+			return $item->category() == 2 && $item->quantity() >= 5;
+		})) > 0;
 	}
 	public function calc(Order $order) {
-		return false;
+		$items = array_filter($order->products(), function($item) {
+			return $item->category() == 2 && $item->quantity() >= 5;
+		});
+		$discounts = array();
+		foreach($items as $item) {
+			array_push($discounts, array(
+				'discount' => 'gift',
+				'description' => "For every products of category 'Switches' (id 2), when you buy five, you get a sixth for free.",
+				'product_id' => $item->id()
+			));
+		}
+		return $discounts;
 	}
 }
