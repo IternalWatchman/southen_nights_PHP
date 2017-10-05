177f9c725501470b63c370435449a83c3a5ff75f
Kauperwood <noblethrow@gmail.com>
2017-10-05 06:32:41 +0100

    phpunit initial

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+require 'autoload.php';
 
 $watchman = new Lex\Watchman();
 
---
+++ tests/DiscountTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DiscountTest extends TestCase {
+
+	public function testOne() {
+		$this->assertEquals(1,1);
+	}
+}
